3ab7bcf0c80af30b1a7c57938d3568662f18b1db
Saqlain Ahmad <saqlainahmed292@gmail.com>
2023-09-12 22:52:04 +0500

    favorite app completed to understand provider

--- lib/favorite/myfavorites.dart
+++ lib/favorite/myfavorites.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_app/favorite/favorite-screen-provider.dart';
-import 'package:provider_app/favorite/myfavorites.dart';
 
-class Favorite extends StatefulWidget {
-  const Favorite({super.key});
+import 'favorite-screen-provider.dart';
+
+class Myfavorite extends StatefulWidget {
+  const Myfavorite({super.key});
 
   @override
-  State<Favorite> createState() => _FavoriteState();
+  State<Myfavorite> createState() => _MyfavoriteState();
 }
 
-class _FavoriteState extends State<Favorite> {
-  List<int> selectedItem = [];
+class _MyfavoriteState extends State<Myfavorite> {
   @override
   Widget build(BuildContext context) {
-    // final favoriteProvider = Provider.of<FavoriteProvider>(context);
+    final favoriteProvider = Provider.of<FavoriteProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Favorite"),
@@ -34,7 +33,7 @@ class _FavoriteState extends State<Favorite> {
       body: Column(children: [
         Expanded(
           child: ListView.builder(
-              itemCount: 50,
+              itemCount: favoriteProvider.selectedItem.length,
               itemBuilder: (context, index) {
                 return Consumer<FavoriteProvider>(
                   builder: (context, value, child) {
