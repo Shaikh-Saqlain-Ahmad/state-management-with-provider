c3c2b623bfbff6f3283fb512cccd68be216316c4
Saqlain Ahmad <saqlainahmed292@gmail.com>
2023-09-12 19:05:39 +0500

    single provider completed

--- lib/screen/count.dart
+++ lib/screen/count.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_app/provider/count_provider.dart';
 
 class Count extends StatefulWidget {
   const Count({super.key});
@@ -10,13 +12,18 @@ class Count extends StatefulWidget {
 class _CountState extends State<Count> {
   @override
   Widget build(BuildContext context) {
+    final countProvider = Provider.of<CountProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(title: Text('Counter')),
-      body: Center(
-        child: Text('0'),
-      ),
+      body: Center(child: Consumer<CountProvider>(
+        builder: (context, value, child) {
+          return Text(value.count.toString());
+        },
+      )),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          countProvider.setcounter();
+        },
         child: Icon(Icons.add),
       ),
     );
